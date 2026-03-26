e8c5841698481c4b3f2758c2de846af252ba1cd6
rudrap18111999 <rudrap18111999@gmail.com>
2026-03-26 20:08:20 +0530

    Update tomcat.sh (1)

--- tomcat.sh
+++ tomcat.sh
@@ -4,8 +4,6 @@ tar -zxvf apache-tomcat-10.1.52.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-10.1.52/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-10.1.52/conf/tomcat-users.xml
 sed -i '58  a\<user username="tomcat" password="root123" roles="manager-gui, manager-script"/>' apache-tomcat-10.1.52/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-10.1.52/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-10.1.52/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-10.1.52/webapps/manager/META-INF/context.xml
 sed -i '22d' apache-tomcat-10.1.52/webapps/manager/META-INF/context.xml
 sh apache-tomcat-10.1.52/bin/startup.sh
